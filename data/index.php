433c3c498d14860b8946fa13bfe93188e34625ff
doghouch <aaronryan2001@gmail.com>
2016-05-28 22:48:52 -0400

    Update index.php

---
+++ data/index.php
@@ -0,0 +1,6 @@
+<?php
+// Welcome to the void of uselessness! No content should display here; just keep on moving folks! If you'd like, uncomment the header
+// below to redirect users back to the root directory if they visit here "by mistake."
+
+// header("Location: /");
+?>
